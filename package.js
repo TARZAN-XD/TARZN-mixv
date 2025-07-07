25a5a0401b24f2299dd6119113e77016a0298524
TARZAN-XD <tkrshqtk@gmail.com>
2025-07-07 17:13:35 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,17 @@
+{
+  "name": "tarzan-hosting",
+  "version": "1.0.0",
+  "description": "استضافة مواقع HTML ثابتة بواسطة طرزان الواقدي",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "engines": {
+    "node": ">=16.0.0"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "multer": "1.4.4",
+    "uuid": "^9.0.0"
+  }
+}
